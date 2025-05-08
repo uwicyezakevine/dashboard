344a8ff61e393a8ad8540ea203f4a5b057334da3
SamIL250 <scamil350@gmail.com>
2025-05-08 12:31:49 +0200

    new features on the right aside

--- components/right_aside.php
+++ components/right_aside.php
@@ -1,4 +1,4 @@
-<div>
+<div class="px-5">
     <!-- //actions -->
     <div class="flex items-center justify-end gap-7 py-5 text-xl">
         <i class="bi bi-question-circle"></i>
@@ -20,6 +20,31 @@
             <p class="text-lg font-bold">Candace Jules</p>
             <p class="my-2 text-sm font-bold text-gray-600">Director of recruiting</p>
             <p class="text-sm text-blue-500"><i class="bi bi-geo-alt-fill"></i> New York, US</p>
+        </div>
+    </div>
+
+    <div class="flex justify-between items-center my-10">
+        <p class="font-bold">Job Posted</p>
+        <div class="grid grid-cols-2 gap-2">
+            <button class="py-1 px-3 bg-blue-500 text-white rounded-md font-bold">Add</button>
+            <button class="py-1 px-3 bg-gray-100 rounded-md font-bold"><i class="bi bi-caret-right"></i></button>
+        </div>
+    </div>
+
+    <div class="bg-orange-400 rounded-md p-3 relative">
+        <div class="flex items-center gap-4 ">
+            <div class="bg-orange-200 p-3 rounded-md text-3xl font-bold text-white">
+                95
+            </div>
+            <div>
+                <p class="font-bold text-white">Sr. Android developer</p>
+                <p class="text-gray-200">Total Applications</p>
+            </div>
+            <div class="absolute top-2 right-2">
+                <?php
+                    require './components/switch.php';
+                ?>
+            </div>
         </div>
     </div>
 
